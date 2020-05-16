d42c73af8ffce15904748b3f6d32c646fe6c6ff5
Darlene Ko <ko.darlene@gmail.com>
2020-05-16 16:22:25 -0500

    Push

--- static/js/app.js
+++ static/js/app.js
@@ -1,58 +1,35 @@
-// // Creat a Function to Get the Data
-// function getData() {
-        // var data;
-//     // Fetch the JSON data and console log it
-//         d3.json("samples.json").then(function(promiseData) {
-//          data = promiseData
-
-//     });
-//   return data
-// }
-
-// const data = getData();
-
+// Fetch the JSON data and console log it
 d3.json('samples.json').then(function(data){
     console.log(data);
+
+    // Create a Function to Build the List of Subject ID Numbers
     function buildInfo(data) {
 
-        // Fetch the JSON data and console log it
-            // d3.json("samples.json").then(function(data) {
-            //     console.log(data);
+        // Grab values from the data json object to build the list of Subect ID Nos.
+        var names = data.names;
+            console.log(names);
         
-        // Promise Pending
-            // const dataPromise = d3.json("samples.json");
-            //     console.log("Data Promise: ", dataPromise);
-    
-        // Grab values from the data json object to build the list of Subect ID Numbers
-            var names = data.names;
-                console.log(names);
-    
-            var static = d3.select("#selDataset")
+        // Define variable to select the Dropdown Menu Option from HTML file and append the Looped through Subject ID Nos.
+        var static = d3.select("#selDataset")
             names.forEach(function(name){
-                static.append("option")
-                .text(name)
-                .attr("value", name)
+            static.append("option")
+            .text(name)
+            .attr("value", name)
             })
-        }
+    }
+    
+    // Create a Function to Build the Bar Chart and Bubble Chart  
     function buildPlots(data,name) {
     
-        // // Fetch the JSON data and console log it
-        //     d3.json("samples.json").then(function(data) {
-        //         console.log(data);
-    
-        // // Promise Pending
-        //     const dataPromise = d3.json("samples.json");
-        //         console.log("Data Promise: ", dataPromise);
-    
-        // Grab values from the data json object to build the Bar Chart and Bubble Chart for first Subject
-            // var sample = data.samples[0];
-        
-            var meta = data.metadata.forEach(function(metadata){
-                if (metadata.id===name){
-                    return(metadata)
-                }
-            })
-            var sample
+        // Grab values from the data json object by matching on Subject ID No. to build the Bar Chart and Bubble Chart
+        var meta = data.metadata.forEach(function(metadata){
+            if (metadata.id===name){
+                return(metadata)
+            }
+        })
+
+        // Define variable to later be utilized after Looping through Entire Row of Data
+        var sample
 
             data.samples.forEach(function(sampleData){
                 if (sampleData.id===name){
@@ -61,170 +38,66 @@ d3.json('samples.json').then(function(data){
             })
     
         // Create the trace for Bar Chart
-            var trace1 = {
-                x: sample.sample_values.slice(0, 10).reverse(),
-                y: sample.otu_ids.slice(0, 10).map(number => `OTU ID ${number}`).reverse(),
-                hovertext: sample.otu_labels.slice(0, 10).reverse(),
-                type: "bar",
-                orientation: "h",
-            };
+        var trace1 = {
+            x: sample.sample_values.slice(0, 10).reverse(),
+            y: sample.otu_ids.slice(0, 10).map(number => `OTU ID ${number}`).reverse(),
+            hovertext: sample.otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h",
+        };
       
         // Create the data array for the Bar Chart
-            var data1 = [trace1];
+        var data1 = [trace1];
       
         // Define the plot layout for the Bar Chart
-            var layout1 = {
-                title: "Top 10 Samples per Subject",
-                margin: {
-                    l: 100,
-                    r: 100,
-                    t: 100,
-                    b: 100
-                }
-            };
+        var layout1 = {
+            title: "Top 10 Samples per Subject",
+            margin: {
+                t: 100,
+                b: 100,
+                l: 100,
+                r: 100
+            }
+        };
           
         // Plot the chart to a div tag with id "bar"
-            Plotly.newPlot("bar", data1, layout1);
+        Plotly.newPlot("bar", data1, layout1);
     
         // Create the trace for Bubble Chart
-            var trace2 = {
-                x: sample.otu_ids,
-                y: sample.sample_values,
-                mode: "markers",
-                marker: {
-                    size: sample.sample_values,
-                    //color = d3.scale.category20c(),
-                    color: sample.otu_ids
-                },
-                hovertext:  sample.otu_labels
-            };
+        var trace2 = {
+            x: sample.otu_ids,
+            y: sample.sample_values,
+            hovertext:  sample.otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample.sample_values,
+                color: sample.otu_ids
+            }
+        };
     
         // Create the data array for the Bubble Chart
-            var data2 = [trace2];
+        var data2 = [trace2];
     
         // Define the plot layout for the Bubble Chart
-            var layout2 = {
-                xaxis:{title: "OTU ID"},
-                height: 600,
-                width: 1000
-            };
+        var layout2 = {
+            title: "All Samples per Subject",
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1200
+        };
     
         // Plot the chart to a div tag with id "bubble"
-            Plotly.newPlot("bubble", data2, layout2); 
+        Plotly.newPlot("bubble", data2, layout2); 
         }
+    
+    // Call the buildInfo Function
     buildInfo(data);
-
+    
+    // Call the buildPlots Function after attaching an Event Listener to select a Subject ID from Dropdown Menu
     d3.select('#selDataset').on('change',function(){
         name =  d3.select(this).property('value');
         console.log(name)
         buildPlots(data,name);
     })
-    
 })
 
-// // Create a Function to Provide Demographic Information
-// function buildInfo() {
-
-//     // Fetch the JSON data and console log it
-//         d3.json("samples.json").then(function(data) {
-//             console.log(data);
-    
-//     // Promise Pending
-//         const dataPromise = d3.json("samples.json");
-//             console.log("Data Promise: ", dataPromise);
-
-//     // Grab values from the data json object to build the list of Subect ID Numbers
-//         var names = data.names;
-//             console.log(names);
-
-//         var static = d3.select("#selDataset")
-//         names.forEach(function(name){
-//             static.append("option")
-//             .text(name)
-//             .attr("value", name)
-//         })
-//     });
-// }
-
-// buildInfo();
-
-// // Create a Function to Build both Bar Chart and Bubble Chart
-// function buildPlots() {
-    
-//     // Fetch the JSON data and console log it
-//         d3.json("samples.json").then(function(data) {
-//             console.log(data);
-
-//     // Promise Pending
-//         const dataPromise = d3.json("samples.json");
-//             console.log("Data Promise: ", dataPromise);
-
-//     // Grab values from the data json object to build the Bar Chart and Bubble Chart for first Subject
-//         var sample = data.samples[0];
-    
-//         var meta = data.metadata.forEach(function(metadata){
-//             if (metadata.id===name){
-//                 return(metadata)
-//             }
-//         })
-
-//     // Create the trace for Bar Chart
-//         var trace1 = {
-//             x: sample.sample_values.slice(0, 10).reverse(),
-//             y: sample.otu_ids.slice(0, 10).map(number => `OTU ID ${number}`).reverse(),
-//             hovertext: sample.otu_labels.slice(0, 10).reverse(),
-//             type: "bar",
-//             orientation: "h",
-//         };
-  
-//     // Create the data array for the Bar Chart
-//         var data1 = [trace1];
-  
-//     // Define the plot layout for the Bar Chart
-//         var layout1 = {
-//             title: "Top 10 Samples per Subject",
-//             margin: {
-//                 l: 100,
-//                 r: 100,
-//                 t: 100,
-//                 b: 100
-//             }
-//         };
-      
-//     // Plot the chart to a div tag with id "bar"
-//         Plotly.newPlot("bar", data1, layout1);
-
-//     // Create the trace for Bubble Chart
-//         var trace2 = {
-//             x: sample.otu_ids,
-//             y: sample.sample_values,
-//             mode: "markers",
-//             marker: {
-//                 size: sample.sample_values,
-//                 //color = d3.scale.category20c(),
-//                 color: sample.otu_ids
-//             },
-//             hovertext:  sample.otu_labels
-//         };
-
-//     // Create the data array for the Bubble Chart
-//         var data2 = [trace2];
-
-//     // Define the plot layout for the Bubble Chart
-//         var layout2 = {
-//             xaxis:{title: "OTU ID"},
-//             height: 600,
-//             width: 1000
-//         };
-
-//     // Plot the chart to a div tag with id "bubble"
-//         Plotly.newPlot("bubble", data2, layout2); 
-//     });
-// }
-
-
-
-
-
-
-// buildPlots();
